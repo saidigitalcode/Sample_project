8f2ba181154224dad0c55e62bb607ce5b229703d
Nandhu2291 <githubsaidigital@gmail.com>
2025-08-01 20:14:37 +0530

    change the api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.160:8000/api/',
+  baseURL: 'http://sample-django-backend.azurewebsites.net/api/',
 });
 
 export default api;
