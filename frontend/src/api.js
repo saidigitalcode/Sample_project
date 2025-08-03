ad7678b49a55d0f452df26c36e5999296f4afb32
Nandhu2291 <githubsaidigital@gmail.com>
2025-08-03 19:29:08 +0530

    frontend change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://backend-server.azurewebsites.net/api/',
+  baseURL: 'https://backend-server.azurewebsites.net/api/',
 });
 
 export default api;
